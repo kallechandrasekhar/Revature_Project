9b62557e71bc836e4a99e7d109836acb9dbb1b8c
chandra <chandrasekharkalle468@gmail.com>
2026-02-26 10:17:55 +0530

    last commit

--- src/main/java/com/revhire/controller/jobseeker/JobSeekerController.java
+++ src/main/java/com/revhire/controller/jobseeker/JobSeekerController.java
@@ -130,7 +130,6 @@ public class JobSeekerController {
         return ApiResponse.success("Profile retrieved", mapToDto(profile));
     }
 
-    // Helper method to map entity to DTO
     private JobSeekerProfileDto mapToDto(JobSeekerProfile profile) {
         JobSeekerProfileDto dto = new JobSeekerProfileDto();
         dto.setId(profile.getId());
